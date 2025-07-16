44f276a104bcd6821a2b4b36db9cbe00d351aa04
Cuong312004 <quoccuongluu03@gmail.com>
2025-07-17 01:23:27 +0700

    add image

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -47,17 +47,6 @@ resource "azurerm_network_security_group" "jenkins_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "Allow-sonerqube_web"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "9000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 resource "azurerm_network_interface_security_group_association" "jenkins_nic_nsg" {
